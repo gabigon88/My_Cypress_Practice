55ccc859a504138b09a9e29bb4ff046557129f16
gabigon88 <bob821114@gmail.com>
2019-12-23 15:29:02 +0800

    完成上傳

---
+++ README.md
@@ -0,0 +1,28 @@
+# My_Cypress_Practice
+練習用Cypress執行web的e2e自動化測試
+
+## Cypress環境架設
+[注意] Cypress會吃Chrome的版本
+
+### 1.安裝Node.js
+Cypress 是用 Javascript 執行的，所以要先安裝 Node.js  
+<a herf="https://nodejs.org/en/">Node.js 官網</a>
+
+### 2.建立cypress目錄
+先新建專案資料夾，開啟CMD，移動專案目錄下建立專案
+```javascript
+  cd [專案資料夾]
+  npm install cypress --save-dev
+```
+
+### 3.啟動Cypress
+確認目前是在專案目錄下，執行指令
+```javascript
+  .\node_modules\.bin\cypress open  --> windows
+```
+
+執行成功後應該可以看到以下視窗  
+<img src="./tutorial01.png"/>  
+
+## 後記
+參照：<a herf="https://www.itread01.com/content/1543765093.html">e2e測試框架之Cypress</a>
--- cypress/integration/Mytest.js
+++ cypress/integration/Mytest.js
@@ -1,20 +1,18 @@
-describe('My First Cypress Test', function () {
-    it('Search for singer!', function () {
+describe('博客來 Search test', function () {
+    it('Search for book!', function () {
+        var keyword = 'python'
+        var regex = new RegExp(keyword, 'i') // case-insensitive
+
         cy.visit("https://www.books.com.tw")
 
         // 如果有跳廣告視窗就關閉廣告視窗
-        try{
-            cy.get("#close_top_banner").click()
-        }
-        catch(e) {
-        }
+        // {force: true} This disables all error checking
+        cy.get("#close_top_banner").click({force: true})
 
-        cy.get("#key").type("python{enter}")
-        
-        cy.wait(1000)
+        cy.get("#key").type(keyword + "{enter}")
 
         cy.get("#searchlist > ul > li:nth-child(1) > h3 > a")
         .should('have.attr', 'title')
-        .and('contain', 'Python')
+        .and('match', regex)
     })
 })
